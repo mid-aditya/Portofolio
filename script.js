04754303415f39c69c72199e370428aa03b798c8
mid-aditya <adityatama780@gmail.com>
2023-09-14 11:02:50 +0700

    update ke 1

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-let sections = document.querySelector('section');
-let navLinks = document.querySelector('header nav a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
-        let Height = sec.offsetHeight;
+        let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + Height) {
-            navLink.forEach(links => {
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
-            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
@@ -36,7 +36,7 @@ ScrollReveal({
     //reset: true,
     distance: '50px',
     duration: 2000,
-    delay: 100
+    delay: 500
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
